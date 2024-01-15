ca70a47948cbf19b0fd228a63c8123b09aa6b7a0
Jack Heuberger <jackheuberger5@gmail.com>
2024-01-15 17:39:36 -0600

    Updated database connection to detect username

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,16 +2,21 @@ import mongoose from "mongoose";
 
 export default async function DBConnect(): Promise<void> {
   try {
-    const user = process.env.MONGO_USER
-    const pass = process.env.MONGO_PASSWORD
-    if (process.env.NODE_ENV === "production") {
-      console.log("prod")
-      const conn = await mongoose.connect(`mongodb://${user}:${pass}@mongo:27017/loanertracker`);
-      console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } else {
-      console.log(`mongodb://${user}:${pass}@localhost:27017/loanertracker`)
-      const conn = await mongoose.connect(`mongodb://${user}:${pass}@localhost:27017/loanertracker`);
-      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    if (process.env.MONGO_USER) {
+      const user = process.env.MONGO_USER
+      const pass = process.env.MONGO_PASSWORD
+      if (process.env.NODE_ENV === "production") {
+        console.log("prod")
+        const conn = await mongoose.connect(`mongodb://${user}:${pass}@mongo:27017/loanertracker`);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+      } else {
+        console.log(`mongodb://${user}:${pass}@localhost:27017/loanertracker`)
+        const conn = await mongoose.connect(`mongodb://${user}:${pass}@localhost:27017/loanertracker`);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+      }
+    }
+    else {
+      const conn = await mongoose.connect(`mongodb://mongo:27017/loanertracker`);
     }
   } catch (err) {
     console.error(err);
